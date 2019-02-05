ee3c0365b4191b476056b6a0816fb90a8ccaada3
gabrielreisn <gabrielreisnf@gmail.com>
2019-02-05 00:08:10 -0200

    adding basic usage for button, removing gitkeep and trying to add travis

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   indexHtml: undefined,
   codeSandbox: false,
+  typescript: true,
+  repository: false,
+  title: 'Paper components',
+  themeConfig: {
+    colors: {
+      primary: 'tomato',
+    },
+  },
 };
